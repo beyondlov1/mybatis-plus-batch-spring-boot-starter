023b4c8b8babce7258972ba2789f9aef24a1ecde
chenshipeng <chenshipeng@ysbang.cn>
2020-07-22 11:24:41 +0800

    init

---
+++ src/main/java/com/beyond/mybatisplus/batch/config/MybatisPlusBatchAutoConfiguration.java
@@ -0,0 +1,18 @@
+package com.beyond.mybatisplus.batch.config;
+
+import com.baomidou.mybatisplus.core.injector.ISqlInjector;
+import com.beyond.mybatisplus.batch.sqlinject.CustomerSqlInjector;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * @author chenshipeng
+ * @date 2020/07/17
+ */
+@Configuration
+public class MybatisPlusBatchAutoConfiguration {
+    @Bean
+    public ISqlInjector sqlInjector(){
+        return new CustomerSqlInjector();
+    }
+}
